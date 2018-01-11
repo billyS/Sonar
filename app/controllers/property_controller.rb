6af228eea221b65f759ddf7aa5492bb191f524b4
billyS <ws52@uni.brighton.ac.uk>
2018-01-11 23:33:38 +0000

    Update property_controller.rb
    removing api key

--- app/controllers/property_controller.rb
+++ app/controllers/property_controller.rb
@@ -4,11 +4,11 @@ class PropertyController < ApplicationController
   end
   def show
     #@location = Homeflow::API::Location.find_by_county_id_and_location_id(params[:county_id], params[:location_id]).location
-    response = HTTParty.get("http://index1.homeflow.co.uk/places?api_key=77467477edfd2689cd77796a2c4b019f&search[name]="+  params[:location])
+    response = HTTParty.get("http://index1.homeflow.co.uk/places?api_key=xxxxxxxxxxxxxxx&search[name]="+  params[:location])
 
     @test =  response["result"]["locations"]["elements"][0]["place_id"]
 
-    response2 = HTTParty.get("http://index1.homeflow.co.uk/properties?api_key=77467477edfd2689cd77796a2c4b019f&search[place][id]="+@test+"&[search][channel]=sales")
+    response2 = HTTParty.get("http://index1.homeflow.co.uk/properties?api_key=xxxxxxxxxxxxx&search[place][id]="+@test+"&[search][channel]=sales")
     @test2 = response2["result"]["properties"]
 
     render json: JSON.pretty_generate(@test2)
