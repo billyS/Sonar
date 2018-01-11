5edae3bf1c5015751eb585c7143358a12e5c6dc9
user10 <user10@DESKTOP-JED5T71.Home>
2018-01-11 20:39:24 +0000

    removing api key

--- TechTest/app/controllers/property_controller.rb
+++ TechTest/app/controllers/property_controller.rb
@@ -1,6 +1,6 @@
 class PropertyController < ApplicationController
   def new
-    response = HTTParty.get("http://index1.homeflow.co.uk/places?api_key=77467477edfd2689cd77796a2c4b019f&search[name]=brighton")
+    response = HTTParty.get("http://index1.homeflow.co.uk/places?api_key=xxxxxxxxxxxx&search[name]=brighton")
     @test =  response["result"]["locations"]["elements"]
 
   end
